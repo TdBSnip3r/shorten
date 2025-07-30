2ff5c9fd8e12a1ea4964e99e52992a94c41b3a61
_Retro_ <39262705+TdBSnip3r@users.noreply.github.com>
2025-07-30 18:56:18 +0200

    Rimozione delle pagine "Contattaci" e "Chi Siamo" dal progetto, semplificando la struttura delle rotte e migliorando la gestione dei componenti. Aggiornamento della navigazione per riflettere queste modifiche.

--- components/common/AuthHeader/AuthNavigation.tsx
+++ components/common/AuthHeader/AuthNavigation.tsx
@@ -14,7 +14,6 @@ const AuthNavigation : React.FC<AuthNavigationProps> = ({ isMobile = false }) =>
         <div className={`flex  gap-4 ${isMobile ? 'flex-col' : 'flex-row'}`}>
           <Link onClick={() => setIsOpen(false)} href="/dashboard" className={pathname === '/dashboard' ? 'text-blue-500' : 'text-gray-500'}>Dashboard</Link>
           <Link onClick={() => setIsOpen(false)} href="/dashboard/links" className={pathname === '/dashboard/links' ? 'text-blue-500' : 'text-gray-500'}>Links</Link>
-          <Link onClick={() => setIsOpen(false)} href="/dashboard/contacts" className={pathname === '/dashboard/contacts' ? 'text-blue-500' : 'text-gray-500'}>Contattaci</Link>
           {
             isMobile &&
             <p className="text-gray-500 text cursor-pointer" onClick={logout}>Logout</p>
--- components/layout/Header/Navigation.tsx
+++ components/layout/Header/Navigation.tsx
@@ -11,9 +11,7 @@ const Navigation: React.FC<NavigationProps> = ({ isMobile = false }) => {
     return (
         <div className={`flex  gap-4 ${isMobile ? 'flex-col' : 'flex-row'}`}>
             <Link onClick={() => setIsOpen(false)} href="/landing" className={pathname === '/landing' ? 'text-blue-500' : 'text-gray-500'}>Home</Link>
-            <Link onClick={() => setIsOpen(false)} href="/landing/about" className={pathname === '/landing/about' ? 'text-blue-500' : 'text-gray-500'}>Chi Siamo</Link>
             <Link onClick={() => setIsOpen(false)} href="/landing/services" className={pathname === '/landing/services' ? 'text-blue-500' : 'text-gray-500'}>Servizi</Link>
-            <Link onClick={() => setIsOpen(false)} href="/landing/contacts" className={pathname === '/landing/contacts' ? 'text-blue-500' : 'text-gray-500'}>Contatti</Link>
             {
                 isMobile &&
                 <Link onClick={() => setIsOpen(false)} href="/landing/login" className={pathname === '/landing/login' ? 'text-blue-500' : 'text-gray-500'}>Login</Link>

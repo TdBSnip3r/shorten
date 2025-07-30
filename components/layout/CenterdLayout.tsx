21db7ca49078f6775c1a418491bb746fbe7abd05
_Retro_ <39262705+TdBSnip3r@users.noreply.github.com>
2025-07-30 18:34:32 +0200

    Modifica del layout del componente CenteredLayout per migliorare la gestione degli spazi. Aggiunto padding verticale per un aspetto più equilibrato.

--- components/layout/CenterdLayout.tsx
+++ components/layout/CenterdLayout.tsx
@@ -2,7 +2,7 @@
 
 const CenteredLayout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className="flex flex-col items-center justify-center h-screen px-4">
+        <div className="flex flex-col items-center justify-center min-h-screen px-4 py-8">
             {children}
         </div>
     )
